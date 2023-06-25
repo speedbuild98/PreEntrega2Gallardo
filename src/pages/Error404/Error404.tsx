dfa7ab1b917b84a8ac5e756e9e85f108d19358eb
speedbuild98 <lautaropectm98@gmail.com>
2023-06-25 11:09:55 -0300

    Fixed ERROR title in mobile

--- src/pages/Error404/Error404.tsx
+++ src/pages/Error404/Error404.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Error404 = () => {
   return (
-    <main className="bg-hexagonal h-screen flex flex-col items-center justify-center">
+    <main className="bg-hexagonal h-screen flex flex-col items-center justify-center text-center">
       <h1 className="text-[90px] font-black text-yellow">ERROR 404</h1>
       <p className="font-black uppercase">Page not found</p>
       <button className="btn mt-10">
